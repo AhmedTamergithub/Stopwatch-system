9b1aec1c44ff9ad020f8db6ddf30d02727515c73
Ahmed Tamer <143439432+AhmedTamergithub@users.noreply.github.com>
2024-08-17 17:48:56 +0300

    Project 1

--- sevensegment/Debug/sevensegment_program.d
+++
@@ -1,8 +0,0 @@
-sevensegment_program.o sevensegment_program.o: ../sevensegment_program.c \
-  ../BIT_Calc.h ../STD_Types.h ../sevensegment_interface.h
-
-../BIT_Calc.h:
-
-../STD_Types.h:
-
-../sevensegment_interface.h:
